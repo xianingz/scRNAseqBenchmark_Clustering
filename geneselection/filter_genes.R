c79e5d5dd684d8453ccc2a032f7ca2aebbe07af9
Xianing Zheng <zhengxianing@gmail.com>
2019-03-21 11:28:51 -0400

    add geneselection functions

---
+++ geneselection/filter_genes.R
@@ -0,0 +1,16 @@
+suppressPackageStartupMessages({
+  library(SingleCellExperiment)
+  library(scater)
+  library(scran)
+})
+
+function postprocessing(sce_filt){
+  sce_filt <- calculateQCMetrics(sce_filt)
+  sce_filt <- computeSumFactors(sce_filt, sizes = pmin(ncol(sce_filt), seq(20, 120, 20)), min.mean = 0.5)
+  sce_filt <- sce_filt[, sizeFactors(sce_filt) > 0]
+  sce_filt <- normalise(sce_filt, exprs_values = "counts", return_log = TRUE)
+  sce_filt <- normalise(sce_filt, exprs_values = "counts", return_log = FALSE)
+  sce_filt <- runTSNE(sce_filt, exprs_values = "logcounts", perplexity = 10)
+  sce_filt
+}
+
