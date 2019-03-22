df7ce69bddb0ca30d346156721b8e111002ee234
Xianing Zheng <zhengxianing@gmail.com>
2019-03-22 13:37:55 -0400

    updates code

--- SimudataGeneration.R
+++ SimudataGeneration.R
@@ -1,53 +1,56 @@
 #Simulation Data Generation
 
 ## Define parameter spaces
-dropouts <- c(2:6)*0.5
-libsizes <- c(10:15)*0.5
-libscales <- c(1:5)*0.1
+dropouts <- seq(6,0,-2)*0.5
+libsizes <- c(11:14)
+libscales <- c(4:1)*0.1
 
 groupequal <- rep(1/4,4)
 groupunequal1 <- c(1/3,1/3,1/6,1/6)
 groupunequal2 <- c(3/8,1/4,1/4,1/8)
 groupmatrix <- cbind(groupequal, groupunequal1, groupunequal2)
 
-distequal <- c(0.005, 0.0075, 0.01, 0.0125, 0.015)
-distunequal1 <- c(0.0025,0.0025,0.005,0.01) # * 1:5
-distunequal2 <- c(0.0025,0.0025,0.005,0.005) # * 1:5
+#distequal <- c(0.0075, 0.01, 0.0125, 0.015)
+distequal <- c(0.01, 0.015, 0.02, 0.025)
+distunequal1 <- c(0.005,0.005,0.01,0.015) # * (1,2.5,0.5)
+distunequal2 <- c(0.015,0.01,0.005,0.005) # * (1,2.5,0.5)
 
-distmatrix <- matrix(0,nrow=4,ncol=15)
-for(i in c(1:5)){
+distmatrix <- matrix(0,nrow=4,ncol=12)
+for(i in c(1:4)){
   distmatrix[,i] = rep(distequal[i],4)
 }
-for(i in (1:5)){
-  distmatrix[,i+5] = distunequal1 * i
+for(i in (1:4)){
+  distmatrix[,i+4] = distunequal1 * (i+1)/2
 }
-for(i in (1:5)){
-  distmatrix[,i+10] = distunequal2 * i
+for(i in (1:4)){
+  distmatrix[,i+8] = distunequal2 * (i+1)/2
 }
 
 #define code
-clusterdist <- c(paste0("EDA",c(1:5)), paste0("UDB",c(1:5)),paste0("UDC",c(1:5)))
+clusterdist <- c(paste0("EDA",c(1:4)), paste0("UDB",c(1:4)),paste0("UDC",c(1:4)))
 clustersize <- c("ESA","USB","USC")
-libsize <- paste0("LS",c(1:5))
-libscale <- paste0("LC",c(1:5))
-dropout <- paste0("DO",c(1:5))
+libsize <- paste0("LS",c(1:4))
+libscale <- paste0("LC",c(1:4))
+dropout <- paste0("DO",c(1:4))
 
 library(splatter)
 library(scater)
 
-for(i in c(1:15)){
-  for(j in c(1:3)){
-    for(m in c(1:5)){
-      for(n in c(1:5)){
-        for(x in c(1:5)){
+
+##3/22/2019 Only for equal cluster distance 4*2*4*4*4 = 512
+for(i in c(1:4)){ ## cluster distance, 4 degrees of difficulty
+  for(j in c(1:2)){  ## cluster size, only one unequal is included beacuase it symmetric
+    for(m in c(1:4)){  ## library size
+      for(n in c(1:4)){  ## library scale
+        for(x in c(1:4)){  ## dropout rate
           params <- newSplatParams()
           params <- setParam(params, "seed",123)
           params <- setParam(params, "batchCells",400)
           params <- setParam(params, "nGenes",10000)
-          params <- setParam(params, "de.prob",distmatrix[,i])
-          params <- setParam(params, "group.prob", groupmatrix[,j])
+          params <- setParam(params, "group.prob", as.vector(groupmatrix[,j]))
+          params <- setParam(params, "de.prob", c(distmatrix[,i]))
           params <- setParam(params, "lib.loc", libsizes[m])
-          params <- setParam(params, "lib.scale", libsizes[n])
+          params <- setParam(params, "lib.scale", libscales[n])
           params <- setParam(params, "dropout.mid", dropouts[x])
           sim <- splatSimulate(params,method = "groups")
           name <- paste(clusterdist[i], clustersize[j],libsize[m],libscale[n],dropout[x],sep = "_")
@@ -59,3 +62,4 @@ for(i in c(1:15)){
     }
   }
 }
+
--- preprocessing.sh
+++ preprocessing.sh
@@ -1,6 +1,7 @@
-for filename in ./Data/Simulations/*; do
+for filename in ./Data/Simulations_test/*; do
   name=${filename##*/};
   base=${name%.rds}
   echo ${base}_pre.html;
-  Rscript -e "rmarkdown::render('Preprocessing.Rmd', params = list(dataset = '$name'), clean = TRUE,output_file='$./Data/data_pre/${base}_pre.html')"
+  mkdir -p ./Data/data_pre_html
+  Rscript -e "rmarkdown::render('Preprocessing.Rmd', params = list(dataset = '$name'), clean = TRUE,output_file='./Data/data_pre_html/${base}_pre.html')"
 done
